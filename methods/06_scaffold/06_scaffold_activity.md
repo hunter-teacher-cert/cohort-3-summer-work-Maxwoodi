2ad5a06cc2b1138516eb25720d7214bd4137441b
mecpsTealsJava <96259626+Maxwoodi@users.noreply.github.com>
2022-07-21 19:56:09 +0000

    Updated o6_scaffold task

---
+++ methods/06_scaffold/06_scaffold_activity.md
@@ -0,0 +1,32 @@
+Team Builders: Maxwell Yearwood & Joel Bianchi
+
+__TEALS APCSA LESSON__
+* __[Lesson 4.03 — Printing, Searching, & Testing for Equality](https://tealsk12.github.io/apcsa-public/Unit4/Lesson-403.md.html)__
+* [TEALS APCSA Curriculum Map](https://tealsk12.github.io/apcsa-public/SUMMARY-Endorsed.md)
+
+
+# Writing Array Methods Task
+
+## Student Instructions
+* Join assigned partner
+* Setup a multiplayer starter code
+* ___See starter code here: https://github.com/hunter-teacher-cert/cohort-3-summer-work-JABianchi/blob/master/methods/06_scaffold_arrays.java___
+* Complete at least the BASIC & INTERMEDIATE methods
+* The teacher will assign you a method to present on
+  * (Google Slides Template link) 
+
+## Methods to Write
+Methods arranged in increasingly difficulty:
+
+8 Methods for Students to write (1 for a String, 1 for an Array):
+
+BASIC
+* Printing an String/Array
+
+INTERMEDIATE
+* Searching & Replacing in a String/Array
+* Testing for Equality of a String/Array
+
+ADVANCED
+* Reversing an String/Array
+
---
+++ methods/06_scaffold/06_scaffold_arrays.java
@@ -0,0 +1,96 @@
+public class 06_scaffold_arrays{
+
+  public static void main(String[] ants ){
+
+    /* Test code to be provided */
+    //1. Printing
+
+    //2. Searching & Replacing
+
+    //3. 
+    
+    //4. Testing Equality
+
+    //5. String Traversal
+
+    
+  }
+
+
+  //1a. Print all the characters in a String
+  public static void printLetters(String word){
+
+    //loop through each letter in the String
+
+      //get each letter
+
+      //print each letter
+
+    
+  }
+  
+  //1b. Print all the elements of the array
+  public static void printArray(int[] arr){
+
+    
+    
+  }
+
+  //2a. Search a String for a letter and replace
+  public static void searchReplaceString(String word, String searchLetter, String replaceLetter){
+
+
+    
+  }
+  
+  //2b. Searching & Replacing
+  public static void searchReplaceArray(int[] arr, int searchNum, int replaceNum){
+
+
+    
+  }
+
+  //3a. Check if 2 Strings have the same letters
+  public static boolean isSameString(String word1, String word2){
+
+    //check if the words have the same length
+
+
+    //if so, loop through each index
+
+
+      //compare the letters from each word
+
+
+        //return false if letters aren't the same
+
+
+    //return true if the whole words have the same letters
+    
+  }
+  
+  //3b. Test if 2 arrays have the same values
+  public static boolean isSame(int[] arr1, int[] arr2){
+
+
+    
+  }
+
+  //4a. Reverse a word
+  public static String reverseString(String word){
+
+
+    
+  } 
+  
+  //4b. Reverse the given Array
+  public static int[] reverseArray(int[] arr){
+
+
+    
+  }
+  
+
+  
+  
+}
